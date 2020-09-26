e82562f1aec2ae9ff8f1a25aa1b29cf65940b228
andy <andy@ycn.cc>
2020-09-26 13:17:41 +0800

    first blood

---
+++ react/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello React1</h1>
+    </>
+  )
+}
+
+render(<App />, document.getElementById('app'))
